20d773d551abbc4d7e60921b1440699517dc982a
nayanbonde1 <abc@gmail.com>
2026-02-25 03:40:35 +0530

    Clean repo: removed all old files

--- src/services/payment.service.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-const processPayment = (bookingId, amount) => api.post("/payments", { bookingId, amount });
-
-const PaymentService = {
-    processPayment,
-};
-
-export default PaymentService;
